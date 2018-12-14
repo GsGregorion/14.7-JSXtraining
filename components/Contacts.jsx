831a92cd7f98d88ceef657e6f14502257f6a5e37
GsGregorion <41581230+GsGregorion@users.noreply.github.com>
2018-12-14 02:23:23 +0100

    Update Contacts.jsx

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -7,7 +7,9 @@ var Contacts = React.createClass({
         return (
           <ul className="contactsList">
             {this.props.items.map(function(contact) {
-                return <Contact item={contact} key={contact.id}/>
+                return 
+                    <Contact item={contact} key={contact.id}>
+                    </Contact>    
                 })
             }
           </ul>  
